98544d9fd2454475a99c7d56044279636efeb8c1
Anastasiia <lobash.n@gmail.com>
2025-08-15 16:07:35 +0300

    style: add css for teacherCard

--- src/components/TeacherCard/TeacherCard.jsx
+++ src/components/TeacherCard/TeacherCard.jsx
@@ -1,5 +1,8 @@
 import css from "./TeacherCard.module.css";
-import React from "react";
+import { IoIosStar } from "react-icons/io";
+
+import { IoBookOutline } from "react-icons/io5";
+import heart from "../../assets/heart.png";
 
 export default function TeacherCard({ teacher }) {
   const {
@@ -18,33 +21,74 @@ export default function TeacherCard({ teacher }) {
 
   return (
     <div className={css.card}>
-      <img src={avatar_url} alt={name} className={css.avatar} />
-
-      <div className={css.infoTop}>
-        <p>Language</p>
-        <p>
-          Lessons online | Lessons done: {lessons_done} | Rating: {rating} |
-          Price / 1 hour:{price_per_hour}$
-        </p>
+      <div className={css.avatarWrapper}>
+        <div className={css.avatarBorder}>
+          <img src={avatar_url} alt={name} className={css.avatar} />
+        </div>
+        <span className={css.online}></span>
       </div>
+      {/* =============================================================== */}
+      <div className={css.textWrapper}>
+        <div className={css.infoTop}>
+          <div className={css.name}>
+            <p className={css.textLeft}>Language</p>
+            <p className={css.surname}>
+              {name} {surname}
+            </p>
+          </div>
 
-      <div className={css.infoMain}>
-        <h3>
-          {name} {surname}
-        </h3>
-        <p>
-          <strong>Speaks:</strong> {languages.join(", ")}
-        </p>
-        <p>{lesson_info}</p>
-        <p>{conditions}</p>
+          <div className={css.textRight}>
+            <div className={css.wrap}>
+              <p className={css.onlineBox}>
+                <span className={css.inlineIconText}>
+                  <IoBookOutline className={css.iconBook} /> Lessons online{" "}
+                </span>
 
-        <p>{reviews}</p>
-        <div className={css.levels}>
-          {levels.map((level, idx) => (
-            <span key={idx} className={css.levelTag}>
-              {level}
-            </span>
-          ))}
+                <span className={css.inlineIconText}>
+                  {"   | Lessons done:  "}
+                  {lessons_done}
+                  {"  |  "}
+                </span>
+
+                <span className={css.inlineIconText}>
+                  <IoIosStar className={css.iconStar} /> Rating: {rating}
+                </span>
+              </p>
+              <p>
+                {"  |  "}
+                {"Price / 1 hour:"}
+                <span className={css.prices}>{price_per_hour}$</span>
+              </p>
+            </div>
+            <img src={heart} alt="heart" />
+          </div>
+        </div>
+        {/* ================================================================ */}
+        <div className={css.infoMain}>
+          <p>
+            <strong>Speaks:</strong> {languages.join(", ")}
+          </p>
+          <p>{lesson_info}</p>
+          <p>{conditions}</p>
+
+          <div className={css.reviews}>
+            {reviews.map((review, idx) => (
+              <div key={idx} className={css.reviewCard}>
+                <p>
+                  <strong>{review.reviewer_name}</strong> (
+                  {review.reviewer_rating}★)
+                </p>
+                <p>{review.comment}</p>
+              </div>
+            ))}
+          </div>
+          <div className={css.levels}>
+            {levels.map((level, idx) => (
+              <span key={idx} className={css.levelTag}>
+                {level}
+              </span>
+            ))}
+          </div>
         </div>
       </div>
     </div>
--- src/pages/Teachers/Teachers.jsx
+++ src/pages/Teachers/Teachers.jsx
@@ -1,6 +1,6 @@
-// import { useEffect, useState } from "react";
-// import { ref, get } from "firebase/database";
-// import { db } from "../../services/firebase.js";
+import { useEffect, useState } from "react";
+import { ref, get } from "firebase/database";
+import { db } from "../../services/firebase.js";
 import css from "../../pages/Teachers/Teachers.module.css";
 import Container from "../../components/Container/Container";
 import Filters from "../../components/Filters/Filters";
@@ -8,11 +8,36 @@ import TeachersList from "../../components/TeachersList/TeachersList";
 import LoadMoreButton from "../../components/LoadMoreButton/LoadMoreButton.jsx";
 
 export default function Teachers() {
+  const [teachers, setTeachers] = useState([]);
+
+  useEffect(() => {
+    async function fetchTeachers() {
+      try {
+        const snapshot = await get(ref(db, "teachers"));
+
+        if (snapshot.exists()) {
+          const data = snapshot.val();
+          const formatted = Object.entries(data).map(([id, teacher]) => ({
+            id,
+            ...teacher,
+          }));
+          setTeachers(formatted);
+        } else {
+          console.log("No teachers found");
+        }
+      } catch (error) {
+        console.log("not", error);
+      }
+    }
+
+    fetchTeachers();
+  }, []);
+
   return (
     <div className={css.wrapper}>
       <Container>
         <Filters />
-        <TeachersList />
+        <TeachersList teachers={teachers} />
         <LoadMoreButton />
       </Container>
     </div>
